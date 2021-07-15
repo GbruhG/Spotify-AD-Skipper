ed29e5ac0d35be2a43d7cc62a5eaeb82ae71cba0
Kraachan <87491156+Kraachan@users.noreply.github.com>
2021-07-15 16:18:29 +0000

    Add files via upload
    Main files

---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Threading;
+
+
+namespace SpotifyAntiADD_FINAL
+{
+    public partial class Form1 : Form
+    {
+		globalKeyboardHook gkh = new globalKeyboardHook();
+		SpotifyHandler spotify = new SpotifyHandler();
+
+		string spotifyPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Spotify\\Spotify.exe";
+		[DllImport("user32.dll", SetLastError = true)]
+		public static extern void keybd_event(byte virtualKey, byte scanCode, uint flags, IntPtr extraInfo);
+
+		public Form1()
+        {
+            InitializeComponent();
+			gkh.HookedKeys.Add(Keys.F12);
+			gkh.KeyDown += new KeyEventHandler(gkh_KeyDown);
+		}
+
+		void gkh_KeyDown(object sender, KeyEventArgs e)
+		{
+			spotify.close();
+			e.Handled = true;
+		}
+	}
+}
+
+
---
+++ SpotifyHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Management;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace SpotifyAntiADD_FINAL
+{
+    class SpotifyHandler
+    {
+        string spotifyPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Spotify\\Spotify.exe";
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern void keybd_event(byte virtualKey, byte scanCode, uint flags, IntPtr extraInfo);
+
+        public void close()
+        {
+
+            var spotifyProcess = Process.GetProcessesByName("spotify");
+
+            if (spotifyProcess != null)
+            {
+
+                foreach (var spot in spotifyProcess)
+                {
+                    spot.Kill();
+                }
+
+                Process.Start(spotifyPath);
+                Thread.Sleep(4000);
+                keybd_event(0xB3, 0, 0x0001, IntPtr.Zero);
+            }
+        }
+
+
+    }
+}
