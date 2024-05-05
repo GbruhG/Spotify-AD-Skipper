fc164d0eb97df08b3e2d62709d67533d7b80086e
gregor <gregorkracan@gmail.com>
2024-05-05 15:47:58 +0200

    updated

--- SpotifyHandler.cs
+++ SpotifyHandler.cs
@@ -12,6 +12,7 @@ namespace SpotifyAntiADD_FINAL
 {
     class SpotifyHandler
     {
+        // gets the spotify exe path
         string spotifyPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Spotify\\Spotify.exe";
 
         [DllImport("user32.dll", SetLastError = true)]
@@ -19,20 +20,40 @@ namespace SpotifyAntiADD_FINAL
 
         public void close()
         {
-
-            var spotifyProcess = Process.GetProcessesByName("spotify");
-
-            if (spotifyProcess != null)
+            while (true)
             {
+                // gets all the spotify processes
+                var spotifyProcess = Process.GetProcessesByName("spotify");
 
-                foreach (var spot in spotifyProcess)
+                // checks if spotify is running
+                if (spotifyProcess != null)
                 {
-                    spot.Kill();
+                    // loop through all the spotify processes
+                    foreach(var process in spotifyProcess)
+                    {
+                        // checks if the spotify window title contains "Advertisement", if it does that means an ad is running
+                        if (process.MainWindowTitle.Contains("Advertisement", StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            // loop through all the spotify processes and kills them
+                            foreach (var spot in spotifyProcess)
+                            {
+                                spot.Kill();
+                            }
+
+                            // starts spotify
+                            Process.Start(spotifyPath);
+
+                            // waits for spotify to load
+                            Thread.Sleep(4000);
+
+                            // presses keyboard button (PLay Media) to automatically resume playing the song
+                            keybd_event(0xB3, 0, 0x0001, IntPtr.Zero);
+                        }
+                    }
                 }
 
-                Process.Start(spotifyPath);
-                Thread.Sleep(4000);
-                keybd_event(0xB3, 0, 0x0001, IntPtr.Zero);
+                // waits for 1 second before checking if an ad is running
+                Thread.Sleep(1000);
             }
         }
 
